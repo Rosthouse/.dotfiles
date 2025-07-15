70093a009d82d3e68319b9ac17a7239f3e4ad9b8
rosthouse <rosthouse@gmail.com>
2025-07-15 16:59:37 +0200

    Adding nvim tree

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,11 @@ require("config.conf")
 
 vim.opt.langmap = "ü[,¨],è{,!}"
 
+
+-- Disable netrw
+vim.g.loaded_netrw       = 1
+vim.g.loaded_netrwPlugin = 1
+
 -- Plugins
 require("config.lazy")
 
--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,4 +1,11 @@
 
 local builtin = require('telescope.builtin')
+local nvimtree = require('nvim-tree.api')
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+
+
+-- nvim-tree
+vim.keymap.set('n', '<leader>e', ":NvimTreeToggle<CR>", { silent = true, noremap = true })
+vim.keymap.set("n", "h", nvimtree.tree.close,        { desc = "Close" })
+vim.keymap.set("n", "H", nvimtree.tree.collapse_all, { desc = "Collapse All" })

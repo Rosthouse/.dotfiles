fc5dbc64ba4466d6b0fb00424439c869cd36ff4e
rosthouse <rosthouse@gmail.com>
2025-07-16 22:42:05 +0200

    Working on getting code completion to worWorking on getting code completion to workk

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,15 +1,15 @@
 return ({
   "mason-org/mason.nvim",
-  opts = {
-    ui = {
-      icons = {
-        package_installed = "✓",
-        package_pending = "➜",
-        package_uninstalled = "✗"
-      }
-    }
-  },
+  opts = { },
   config = function () 
-    print("Configuring mason")
-  end
+    require("mason").setup({
+        ui = {
+            icons = {
+                package_installed = "✓",
+                package_pending = "➜",
+                package_uninstalled = "✗"
+            }
+        }
+    })
+  end,
 })

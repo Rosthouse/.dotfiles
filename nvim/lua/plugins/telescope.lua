2ff5378ef3b7c8662ee6433e7566c90742382b1b
rosthouse <rosthouse@gmail.com>
2025-07-20 13:49:22 +0200

    Working on stuff

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,5 +4,7 @@ return {
   config = function()
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+    vim.keymap.set('n', '<leader>fb', builtin.buffers,    { desc = 'Telescope find buffers' })
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep,  { desc = 'Telescope find buffers' })
   end,
 }
--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -5,7 +5,6 @@ return {
     {
       "<leader>-",
       function()
-        print("which key pressed")
         require("which-key").show({ global = false })
       end,
       desc = "Buffer Local Keymaps (which-key)"

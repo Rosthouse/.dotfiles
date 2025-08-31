8c7f4255fe8311a7dc060288057520d0d87c9b08
Rosthouse <rosthouse@gmail.com>
2025-08-31 16:56:24 +0200

    Fixing some rights changes in install script

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 ## This should install all apps needed to make these configs work
 exec dnf install blueman dunst git grim hyprland kanshi nvim pavucontrol stow tmux waybar wf-recorder wireplumber wofi swaynag
 ## Make required scripts executable
-exec chmod +x ./dot-config/waybar/scripts/audio_changer.py
+exec chmod +x $SCRIPT_DIR/dot-config/waybar/scripts/audio_changer.py
 ## This links all files (not excluded by .stow-local-ignore) to your home directory
 ## Note that the directory 'dot-config' will map to $HOME/.config/
 exec stow -R --dotfiles -t $HOME $SCRIPT_DIR

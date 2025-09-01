fdaaaba7c1de8c986628852064bee516ec3a91f8
rosthouse <rosthouse@gmail.com>
2025-09-01 07:03:36 +0200

    Extending telescope and adding git plugin

--- dot-config/nvim/lua/plugins/telescope.lua
+++ dot-config/nvim/lua/plugins/telescope.lua
@@ -1,32 +1,37 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope find buffers" })
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope find buffers" })
-			vim.keymap.set("n", "<leader>ft", builtin.commands, { desc = "Telescope find commands" })
-		end,
-	},
-	{
-		"EthanJWright/vs-tasks.nvim",
-		dependencies = {
-			"nvim-lua/popup.nvim",
-			"nvim-lua/plenary.nvim",
-			"nvim-telescope/telescope.nvim",
-		},
-		config = function()
-			local telescope = require("telescope")
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope find buffers" })
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope find buffers" })
+      vim.keymap.set("n", "<leader>ft", builtin.commands, { desc = "Telescope find commands" })
+      vim.keymap.set("n", "<leader>fq", builtin.quickfix, { desc = "Telescope quickfix list" })
+      vim.keymap.set("n", "<leader>fj", builtin.jumplist, { desc = "Telescope jump list" })
+      vim.keymap.set("n", "<leader>fli", builtin.lsp_references, { desc = "Telescope LSP References" })
+      vim.keymap.set("n", "<leader>fli", builtin.lsp_incoming_calls, { desc = "Telescope LSP Incoming Calls" })
+      vim.keymap.set("n", "<leader>fli", builtin.lsp_outgoing_calls, { desc = "Telescope LSP Outgoing Calls" })
+    end,
+  },
+  {
+    "EthanJWright/vs-tasks.nvim",
+    dependencies = {
+      "nvim-lua/popup.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      local telescope = require("telescope")
 
-			vim.keymap.set("n", "<leader>ta", telescope.extensions.vstask.tasks, { desc = "List Tasks" })
-			--vim.keymap.set('n', '<leader>ta', telescope.extensions.vstask.inputs()<CR>
-			--vim.keymap.set('n', '<leader>ta', telescope.extensions.vstask.jobs()<CR>
-			--vim.keymap.set('n', '<leader>ta', telescope.extensions.vstask.clear_inputs()<CR>
-			--vim.keymap.set('n', '<leader>ta', telescope.extensions.vstask.cleanup_completed_jobs()<CR>
-			--vim.keymap.set('n', '<leader>ta', telescope.extensions.vstask.launch()<cr>
-			--vim.keymap.set('n', '<leader>ta', telescope.extensions.vstask.command()<cr>
-		end,
-	},
+      vim.keymap.set("n", "<leader>ta", telescope.extensions.vstask.tasks, { desc = "List Tasks" })
+      --vim.keymap.set('n', '<leader>ta', telescope.extensions.vstask.inputs()<CR>
+      --vim.keymap.set('n', '<leader>ta', telescope.extensions.vstask.jobs()<CR>
+      --vim.keymap.set('n', '<leader>ta', telescope.extensions.vstask.clear_inputs()<CR>
+      --vim.keymap.set('n', '<leader>ta', telescope.extensions.vstask.cleanup_completed_jobs()<CR>
+      --vim.keymap.set('n', '<leader>ta', telescope.extensions.vstask.launch()<cr>
+      --vim.keymap.set('n', '<leader>ta', telescope.extensions.vstask.command()<cr>
+    end,
+  },
 }

7d3891c5d9987b2bbb6db443aa28080b0748adcc
rosthouse <rosthouse@gmail.com>
2025-11-09 12:37:14 +0100

    Installing niri

--- install.sh
+++ install.sh
@@ -4,13 +4,15 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 ## This should install all apps needed to make these configs work
 # exec dnf install blueman dunst git grim hyprland kanshi nvim pavucontrol stow tmux waybar wf-recorder wireplumber wofi swaynag
-exec dnf install niri nvim tmux
+echo "Installing dependencies"
+exec dnf install niri nvim tmux wofi dunst waybar swaybg wofi kanshi 
 
 
 ## Make required scripts executable
 exec chmod +x $SCRIPT_DIR/dot-config/waybar/scripts/audio_changer.py
 ## This links all files (not excluded by .stow-local-ignore) to your home directory
 ## Note that the directory 'dot-config' will map to $HOME/.config/
+echo "Stowing dotfiles"
 exec stow -R --dotfiles -t $HOME $SCRIPT_DIR
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm

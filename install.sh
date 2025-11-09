56787b57a02e4358b94681c47fa094a6397374ad
rosthouse <rosthouse@gmail.com>
2025-11-09 10:06:16 +0100

    Starting integrating niri

--- install.sh
+++ install.sh
@@ -3,7 +3,10 @@
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 ## This should install all apps needed to make these configs work
-exec dnf install blueman dunst git grim hyprland kanshi nvim pavucontrol stow tmux waybar wf-recorder wireplumber wofi swaynag
+# exec dnf install blueman dunst git grim hyprland kanshi nvim pavucontrol stow tmux waybar wf-recorder wireplumber wofi swaynag
+exec dnf install niri nvim tmux
+
+
 ## Make required scripts executable
 exec chmod +x $SCRIPT_DIR/dot-config/waybar/scripts/audio_changer.py
 ## This links all files (not excluded by .stow-local-ignore) to your home directory

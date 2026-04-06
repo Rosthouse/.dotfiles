d8f955c2edfce1fb75735086b87dff427e935e63
Rosthouse <rosthouse@gmail.com>
2026-04-06 13:25:51 +0200

    Ensuring csharp_ls is installed

--- dot-config/nvim/plugin/lsp.lua
+++ dot-config/nvim/plugin/lsp.lua
@@ -4,22 +4,22 @@ vim.pack.add({
   'https://github.com/mason-org/mason-lspconfig.nvim',
 })
 
-require("mason").setup({
+require('mason').setup({
   registries = {
-    "github:mason-org/mason-registry",
-    "github:Crashdummyy/mason-registry",
+    'github:mason-org/mason-registry',
   },
 })
 
-require("mason-lspconfig").setup({
+require('mason-lspconfig').setup({
     ensure_installed = {
       'bashls',
-      "lua_ls",
-      "pyright",
+      'csharp_ls',
+      'lua_ls',
+      'pyright',
     },
 })
 
-vim.lsp.config("*", {
+vim.lsp.config('*', {
   -- capabilities = capabilities,
-  root_markers = { ".git" },
+  root_markers = { '.git' },
 })

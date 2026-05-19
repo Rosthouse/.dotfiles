aafecf5e586767405e4647510e6061c576b6567c
rosthouse <rosthouse@gmail.com>
2026-05-19 20:40:27 +0200

    Making bash scripts executable

--- dot-bashrc.d/bash_aliases.sh
+++ dot-bashrc.d/bash_aliases.sh
@@ -4,6 +4,7 @@ alias pves='source .venv/bin/activate'
 alias fzp="fzf --preview 'cat {}'"
 alias fkill="ps aux | fzf --preview='echo COMMAND: {11}' | awk '{print \$2}' | xargs kill -9"
 alias prtl=pritunl-client
+alias dni='~/.bashrc.d/dotnet-install.sh'
 
 nsl() {
   qs -c noctalia-shell ipc call $@

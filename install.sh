6698a106904f2ab5317c5533fc33508f2d474556
Rosthouse <rosthouse@gmail.com>
2026-06-14 19:49:58 +0200

    removing dankshell

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 sudo -s <<'END_OF_SUDO'
 	echo "Enabling external repos"
 	dnf --assumeyes copr enable scottames/ghostty
-	dnf --assumeyes copr enable avengemedia/dms
 	dnf --assumeyes copr enable buckaroogeek/Tmux_sesh
 
 	dnf install --assumeyes --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release ## For Noctalia Shell
@@ -41,7 +40,6 @@ sudo -s <<'END_OF_SUDO'
 		python-pip \
 		python3-virtualenv
 
-
   sudo usermod -a -G input patrick
 END_OF_SUDO
 
@@ -54,7 +52,7 @@ chmod +x "$SCRIPT_DIR"/dot-config/userscripts/*
 ## Setting up tools
 
 ## Enabling niri
-systemctl --user add-wants niri.service dms
+systemctl --user add-wants niri.service
 
 ## This links all files (not excluded by .stow-local-ignore) to your home directory
 ## Note that the directory 'dot-config' will map to $HOME/.config/

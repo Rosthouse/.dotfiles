90a9cc42016e0bcc6c387f34f0aa214aa0a8627e
rosthouse <rosthouse@gmail.com>
2026-08-26 08:58:18 +0200

    Adding some new features for git, testing and surrouing stuff

--- dot-config/nvim/plugin/git.lua
+++ dot-config/nvim/plugin/git.lua
@@ -6,8 +6,8 @@ require("gitsigns").setup({
   on_attach = function(bufnr)
     local gitsigns = require('gitsigns')
     vim.keymap.set('n', '<leader>gb', function() gitsigns.blame_line({ full = true }) end, { desc = 'Git Blame', })
-    vim.keymap.set('n', '<leader>gbt', gitsigns.toggle_current_line_blame, { desc = 'Git Blame', })
-  end
+    vim.keymap.set('n', '<leader>gbl', gitsigns.toggle_current_line_blame, { desc = 'Git Blame', })
+  end,
 })
 
 -- Octo.nvim for PR review
@@ -16,5 +16,3 @@ vim.pack.add({
   "https://github.com/nvim-telescope/telescope.nvim",
   "https://github.com/pwntester/octo.nvim"
 })
-
-require("octo").setup()
